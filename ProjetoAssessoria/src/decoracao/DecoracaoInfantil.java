80ba5d533d1929031c9402de09d18171de61007d
Lucas Turci <lucas.turci@gmail.com>
2018-06-20 14:45:05 -0300

    Quase feita a tela de alteracao

--- ProjetoAssessoria/src/decoracao/DecoracaoInfantil.java
+++ ProjetoAssessoria/src/decoracao/DecoracaoInfantil.java
@@ -2,6 +2,9 @@ package decoracao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -47,5 +50,97 @@ public class DecoracaoInfantil {
 			JOptionPane.showMessageDialog(null, "Erro ao inserir decoracao");
 			return false;
 		}
+	}
+	
+	public static List<String> select(String tema) {
+		Connection c = Conexao.getInstance();	
+		String sql = "select * from decoracaoinfantil where tema = (?)";
+		
+		List<String> s = new ArrayList<String>();
+
+		try {
+			PreparedStatement pstm = c.prepareStatement(sql);
+			pstm.setString(1, tema);
+			
+			ResultSet rs = pstm.executeQuery();
+			while (rs.next()){
+				s.add(rs.getString(2)); //adiciona qtdeBaloes
+				s.add(rs.getString(3)); //adiciona pinata
+			}
+			
+			pstm.close();
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erro ao procurar tema");
+		}
+		
+		sql = "select cor from corbaloes where decoracao = (?)";
+		try {
+			PreparedStatement pstm = c.prepareStatement(sql);
+			pstm.setString(1, tema);
+			
+			ResultSet rs = pstm.executeQuery();
+			while (rs.next()){
+				s.add(rs.getString(1));
+			}
+			
+			pstm.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro ao procurar cores de baloes");
+		}
+		
+		return s;
+	}
+	
+	public static void update(String[] form) {
+		Connection c = Conexao.getInstance();
+		
+		String sql = "delete from tiposflores where decoracao = ?";
+		
+		
+		try {
+			//Statement s = c.createStatement();
+			//ResultSet rs = s.executeQuery("select * from decoracaocasamento where tema = 'classico'");
+			//if (rs.next())
+			//	System.out.println(rs.getString(1) + " / " + rs.getString(2));
+			
+			PreparedStatement pstm = c.prepareStatement(sql);
+			pstm.setString(1, form[0]);
+			pstm.execute();
+			
+			//s = c.createStatement();
+			//rs = s.executeQuery("select * from decoracaocasamento where tema = 'classico'");
+			//if (rs.next())
+			//	System.out.println(rs.getString(1) + " / " + rs.getString(2));
+			//rs = s.executeQuery("select * from tiposflores where decoracao = 'classico'");
+			//while (rs.next()) {
+			//	System.out.println(rs.getString(1) + " / " + rs.getString(2));
+			//}
+			
+			sql = "update decoracaocasamento set qtde_flores = ? where tema = ?";
+			pstm = c.prepareStatement(sql);
+			pstm.setString(1, form[1]);
+			pstm.setString(2, form[0]);
+			pstm.execute();
+			
+			sql = "insert into tiposflores(decoracao, tipo_flor) values(?, ?)";
+			pstm = c.prepareStatement(sql);
+			pstm.setString(1, form[0]);
+			
+			for (int i = 2; i < form.length; i++) {
+				pstm.setString(2, form[i]);
+				pstm.execute();
+			}
+			
+			pstm.close();
+
+			JOptionPane.showMessageDialog(null, "Decoracao alterada com sucesso");
+		} catch (Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro ao alterar decoracao");
+		}
+
 	}
 }
--- ProjetoAssessoria/src/decoracao/TelaDecoracaoInfantil.java
+++ ProjetoAssessoria/src/decoracao/TelaDecoracaoInfantil.java
@@ -20,30 +20,34 @@ public class TelaDecoracaoInfantil extends JFrame {
 	private JTextField textFieldQtdeBaloes;
 	
 	JCheckBox chckbxPinata;
-	private JLabel lblDigiteTodosOs;
 	private JButton btnCadastrar;
-	private JLabel lblTipoDeFlor;
-	private JLabel lblQuantidadeDeFlores;
+	private JLabel lblQtdeBaloes;
+	private JLabel lblCadastroDeNovaDecoracaoInfantil;
+	private JLabel lblTema;
+	private JLabel lblQtdBaloes;
+	private JTextField textFieldCorBaloes;
+	private JLabel lblDigiteTodosOs;
+	private JLabel lblCorBaloes;
 	
 	public TelaDecoracaoInfantil() {
-		inserirDecoracaoInfantil();
-		//alterarDecoracaoCasamento();
+		//inserirDecoracaoInfantil();
+		alterarDecoracaoInfantil();
 		//removerDecoracaoCasamento();
 	}
 	public void inserirDecoracaoInfantil() {
 		getContentPane().setLayout(null);
 		
-		JLabel lblCadastroDeNovaDecoracaoInfantil = new JLabel("<html> <center> <p>Cadastro de decoracao de Aniversário Infantil</p> </center> </html>");
+		lblCadastroDeNovaDecoracaoInfantil = new JLabel("<html> <center> <p>Cadastro de decoração de Aniversário Infantil</p> </center> </html>");
 		lblCadastroDeNovaDecoracaoInfantil.setVerticalAlignment(SwingConstants.TOP);
 		lblCadastroDeNovaDecoracaoInfantil.setFont(new Font("Dialog", Font.PLAIN, 20));
 		lblCadastroDeNovaDecoracaoInfantil.setBounds(63, 16, 345, 69);
 		getContentPane().add(lblCadastroDeNovaDecoracaoInfantil);
 		
-		JLabel lblTema = new JLabel("Tema");
+		lblTema = new JLabel("Tema");
 		lblTema.setBounds(49, 81, 70, 15);
 		getContentPane().add(lblTema);
 		
-		JLabel lblQtdBaloes = new JLabel("Quantidade de Balões");
+		lblQtdBaloes = new JLabel("Quantidade de Balões");
 		lblQtdBaloes.setBounds(49, 108, 153, 15);
 		getContentPane().add(lblQtdBaloes);
 		
@@ -57,17 +61,17 @@ public class TelaDecoracaoInfantil extends JFrame {
 		textFieldQtdeBaloes.setBounds(230, 106, 140, 19);
 		getContentPane().add(textFieldQtdeBaloes);
 		
-		JLabel lblCorBaloes= new JLabel("Cores dos baloes");
+		lblCorBaloes= new JLabel("Cores dos balões");
 		lblCorBaloes.setBounds(49, 162, 153, 15);
 		getContentPane().add(lblCorBaloes);
 		
-		JTextField textFieldCorBaloes= new JTextField();
+		textFieldCorBaloes= new JTextField();
 		textFieldCorBaloes.setToolTipText("cor1, cor2");
 		textFieldCorBaloes.setColumns(10);
 		textFieldCorBaloes.setBounds(230, 160, 140, 19);
 		getContentPane().add(textFieldCorBaloes);
 		
-		JLabel lblDigiteTodosOs = new JLabel("(Digite todas as cores desejadas separadas por virgulas)");
+		lblDigiteTodosOs = new JLabel("(Digite todas as cores desejadas separadas por virgulas)");
 		lblDigiteTodosOs.setFont(new Font("Dialog", Font.PLAIN, 9));
 		lblDigiteTodosOs.setBounds(49, 179, 308, 20);
 		getContentPane().add(lblDigiteTodosOs);
@@ -90,40 +94,37 @@ public class TelaDecoracaoInfantil extends JFrame {
 		btnCadastrar.setBounds(356, 228, 117, 25);
 		getContentPane().add(btnCadastrar);
 		
-		chckbxPinata = new JCheckBox("Pinata");
+		chckbxPinata = new JCheckBox("Piñata");
 		chckbxPinata.setBounds(49, 131, 128, 23);
 		getContentPane().add(chckbxPinata);
 		
 	}
 
 	private void changeVisibility(boolean flag) {
-		lblDigiteTodosOs.setVisible(flag);
 		textFieldQtdeBaloes.setVisible(flag);
 		btnCadastrar.setVisible(flag);
-		lblQuantidadeDeFlores.setVisible(flag);
+		lblQtdeBaloes.setVisible(flag);
+		lblCorBaloes.setVisible(flag);
+		chckbxPinata.setVisible(flag);
+		textFieldCorBaloes.setVisible(flag);
 	}
 	
-	public void alterarDecoracaoCasamento() {
+	public void alterarDecoracaoInfantil() {
 		getContentPane().setLayout(null);
 		
-		JLabel lblCadastroDeNova = new JLabel("Alteracao de decoracao de casamento");
-		lblCadastroDeNova.setFont(new Font("Dialog", Font.PLAIN, 20));
-		lblCadastroDeNova.setBounds(64, 33, 409, 15);
-		getContentPane().add(lblCadastroDeNova);
+		JLabel lblAltTitle = new JLabel("<html><center>Alteração de decoração de Aniversário Infantil</center></html>");
+		lblAltTitle.setFont(new Font("Dialog", Font.PLAIN, 20));
+		lblAltTitle.setBounds(35, 6, 409, 74);
+		getContentPane().add(lblAltTitle);
 		
 		JLabel lblTema = new JLabel("Tema");
 		lblTema.setBounds(49, 77, 70, 15);
 		getContentPane().add(lblTema);
 		
-		lblQuantidadeDeFlores = new JLabel("Quantidade de Flores");
-		lblQuantidadeDeFlores.setBounds(49, 141, 153, 15);
-		getContentPane().add(lblQuantidadeDeFlores);
-		lblQuantidadeDeFlores.setVisible(false);
-		
-		lblTipoDeFlor = new JLabel("Tipos de Flor");
-		lblTipoDeFlor.setBounds(49, 168, 153, 15);
-		getContentPane().add(lblTipoDeFlor);
-		lblTipoDeFlor.setVisible(false);
+		lblQtdeBaloes = new JLabel("Quantidade de Balões");
+		lblQtdeBaloes.setBounds(49, 130, 153, 15);
+		getContentPane().add(lblQtdeBaloes);
+		lblQtdeBaloes.setVisible(false);
 		
 		textFieldTema = new JTextField();
 		textFieldTema.setBounds(137, 77, 140, 19);
@@ -132,16 +133,34 @@ public class TelaDecoracaoInfantil extends JFrame {
 		
 		textFieldQtdeBaloes = new JTextField();
 		textFieldQtdeBaloes.setColumns(10);
-		textFieldQtdeBaloes.setBounds(230, 139, 140, 19);
+		textFieldQtdeBaloes.setBounds(230, 127, 140, 19);
 		getContentPane().add(textFieldQtdeBaloes);
 		textFieldQtdeBaloes.setVisible(false);
 		
+		lblCorBaloes= new JLabel("Cores dos balões");
+		lblCorBaloes.setBounds(49, 168, 153, 15);
+		getContentPane().add(lblCorBaloes);
+		
+		textFieldCorBaloes= new JTextField();
+		textFieldCorBaloes.setToolTipText("cor1, cor2");
+		textFieldCorBaloes.setColumns(10);
+		textFieldCorBaloes.setBounds(230, 165, 140, 19);
+		getContentPane().add(textFieldCorBaloes);
+		
+		lblDigiteTodosOs = new JLabel("(Digite todas as cores desejadas separadas por virgulas)");
+		lblDigiteTodosOs.setFont(new Font("Dialog", Font.PLAIN, 9));
+		lblDigiteTodosOs.setBounds(49, 183, 308, 20);
+		getContentPane().add(lblDigiteTodosOs);
+		
+		chckbxPinata = new JCheckBox("Piñata");
+		chckbxPinata.setBounds(49, 215, 128, 23);
+		getContentPane().add(chckbxPinata);
 		
 		btnCadastrar = new JButton("ALTERAR");
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String tema = textFieldTema.getText();
-				String qtde = textFieldQtdeBaloes.getText();
+				//String tema = textFieldTema.getText();
+				//String qtde = textFieldQtdeBaloes.getText();
 
 				//DecoracaoCasamento.update(form);
 			}
@@ -149,12 +168,6 @@ public class TelaDecoracaoInfantil extends JFrame {
 		btnCadastrar.setBounds(356, 228, 117, 25);
 		getContentPane().add(btnCadastrar);
 		btnCadastrar.setVisible(false);
-		
-		lblDigiteTodosOs = new JLabel("(Digite todos os tipos desejados separados por virgulas)");
-		lblDigiteTodosOs.setFont(new Font("Dialog", Font.PLAIN, 9));
-		lblDigiteTodosOs.setBounds(49, 179, 308, 20);
-		getContentPane().add(lblDigiteTodosOs);
-		lblDigiteTodosOs.setVisible(false);
 		
 		textFieldTema.addFocusListener(new FocusListener() {
 			
@@ -172,16 +185,19 @@ public class TelaDecoracaoInfantil extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				
 				String tema = textFieldTema.getText();
-				List<String> rs = DecoracaoCasamento.select(tema);
+				List<String> rs = DecoracaoInfantil.select(tema);
 				if (rs != null && rs.size() > 0) {
 					textFieldQtdeBaloes.setText(rs.get(0));
-					String tipos = "";
-					for (int i = 1; i < rs.size(); i++) {
-						tipos += rs.get(i);
-						if (i != rs.size() - 1) tipos += ", ";
+					chckbxPinata.setSelected(rs.get(1) == "1"? true : false);
+					String cores = "";
+					for (int i = 2; i < rs.size(); i++) {
+						cores += rs.get(i);
+						if (i != rs.size() - 1) cores += ", ";
 					}
 					
 					changeVisibility(true);
+					
+					textFieldCorBaloes.setText(cores);
 					
 				} else {
 					textFieldQtdeBaloes.setText("");
@@ -207,15 +223,15 @@ public class TelaDecoracaoInfantil extends JFrame {
 		lblTema.setBounds(49, 77, 70, 15);
 		getContentPane().add(lblTema);
 		
-		lblQuantidadeDeFlores = new JLabel("Quantidade de Flores");
-		lblQuantidadeDeFlores.setBounds(49, 141, 153, 15);
-		getContentPane().add(lblQuantidadeDeFlores);
-		lblQuantidadeDeFlores.setVisible(false);
+		lblQtdeBaloes = new JLabel("Quantidade de Flores");
+		lblQtdeBaloes.setBounds(49, 141, 153, 15);
+		getContentPane().add(lblQtdeBaloes);
+		lblQtdeBaloes.setVisible(false);
 		
-		lblTipoDeFlor = new JLabel("Tipos de Flor");
+		/*lblTipoDeFlor = new JLabel("Tipos de Flor");
 		lblTipoDeFlor.setBounds(49, 168, 153, 15);
 		getContentPane().add(lblTipoDeFlor);
-		lblTipoDeFlor.setVisible(false);
+		lblTipoDeFlor.setVisible(false);*/
 		
 		textFieldTema = new JTextField();
 		textFieldTema.setBounds(137, 77, 140, 19);
