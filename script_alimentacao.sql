dab821d5782cdd8861ddb1e06b9abcde6f663897
BarbaraCortes <barbaracortes.98@hotmail.com>
2018-06-22 14:26:40 -0300

    scrpts certinhos

--- script_alimentacao.sql
+++ script_alimentacao.sql
@@ -4,17 +4,17 @@ insert into Contratante(CPF, nome, telefone, email, rua, numero, bairro, cep)
 insert into Contratante(CPF, nome, telefone, email, rua, numero, bairro, cep)
 	values('123.123.123-21', 'Maria da Graça', NULL, 'maria@hotmail.com', 'Avenida São Carlos', 1515, 'Centro', NULL);
 
-insert into festa(nro_contrato, pdf_contrato, preco, inicio, fim, tipo, contratante)
-	values('1', NULL, NULL, to_date('25/12/18 19:00', 'dd/mm/yy hh24:mi'), to_date('26/12/18 02:00', 'dd/mm/yy hh24:mi'), 'casamento', '123.123.123-21');
+insert into festa(nro_contrato, pdf_contrato, preco, data, tipo, contratante)
+	values('1', NULL, NULL, to_date('25/12/18 19:00', 'dd/mm/yy hh24:mi'), 'casamento', '123.123.123-21');
 
-insert into festa(nro_contrato, pdf_contrato, preco, inicio, fim, tipo, contratante)
-	values('2', NULL, NULL, to_date('31/12/18 14:00', 'dd/mm/yy hh24:mi'), to_date('31/12/18 19:00', 'dd/mm/yy hh24:mi'), 'aniversario infantil', '123.123.123-21');
+insert into festa(nro_contrato, pdf_contrato, preco, data, tipo, contratante)
+	values('2', NULL, NULL, to_date('31/12/18 14:00', 'dd/mm/yy hh24:mi'), 'aniversario infantil', '123.123.123-21');
 
-insert into festa(nro_contrato, pdf_contrato, preco, inicio, fim, tipo, contratante)
-	values('3', NULL, NULL, NULL, NULL, 'aniversario infantil', '123.123.123-21');
+insert into festa(nro_contrato, pdf_contrato, preco, data, tipo, contratante)
+	values('3', NULL, NULL, NULL, 'aniversario infantil', '123.123.123-21');
 
-insert into festa(nro_contrato, pdf_contrato, preco, inicio, fim, tipo, contratante)
-	values('4', NULL, NULL, NULL, NULL, 'casamento', '123.123.123-21');
+insert into festa(nro_contrato, pdf_contrato, preco, data, tipo, contratante)
+	values('4', NULL, NULL, NULL, 'casamento', '123.123.123-21');
 
 insert into ConvidadosFesta(festa, nome)
 	values('2', 'Lucas');
