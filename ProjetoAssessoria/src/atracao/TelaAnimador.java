694e6def726e73d6feedad749c03cb88f3b4517e
Lucas Turci <lucas.turci@gmail.com>
2018-06-23 14:50:48 -0300

    Comentei tela de animador

--- ProjetoAssessoria/src/atracao/TelaAnimador.java
+++ ProjetoAssessoria/src/atracao/TelaAnimador.java
@@ -15,6 +15,8 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class TelaAnimador extends JFrame {
+	
+	/* Objetos usados pelo swing para a interface da tela */
 	private JTextField textFieldNome;
 	private JTextField textFieldTelefone;
 	private JTextField textFieldEmail;
@@ -27,7 +29,7 @@ public class TelaAnimador extends JFrame {
 	private JButton btnVoltar;
 	private JButton btnCadastrar;
 	private JButton btnPesquisarAnimador;
-	private String[] options = {"Animador", "Banda"};
+	private String[] options = {"Magico", "Palhaco"};
 	
 	
 	public TelaAnimador() {
@@ -38,6 +40,7 @@ public class TelaAnimador extends JFrame {
 		
 	}
 	
+	/* Metodo usado para esconder ou revelar o formulario de insercao ou alteracao de animador */
 	private void changeVisibility(boolean flag) {
 		lblTelefone.setVisible(flag);
 		btnCadastrar.setVisible(flag);
@@ -50,6 +53,7 @@ public class TelaAnimador extends JFrame {
 	
 	public void menuInicial() {
 		
+		/* Configura botao que direciona para a tela de inserir animador */
 		JButton btnInserir = new JButton("Inserir Animador");
 		btnInserir.setBounds(275, 110, 215, 49);
 		btnInserir.addActionListener(new ActionListener() {
@@ -63,6 +67,7 @@ public class TelaAnimador extends JFrame {
 		getContentPane().setLayout(null);
 		getContentPane().add(btnInserir);
 		
+		/* Configura botao que direciona para a tela de alterar animador */
 		JButton btnAlterar = new JButton("Alterar Animador");
 		btnAlterar.setBounds(275, 199, 215, 49);
 		btnAlterar.addActionListener(new ActionListener() {
@@ -75,6 +80,7 @@ public class TelaAnimador extends JFrame {
 		});
 		getContentPane().add(btnAlterar);
 		
+		/* Configura botao que direciona para a tela de remover animador */
 		JButton btnRemover = new JButton("Remover Animador");
 		btnRemover.setBounds(275, 288, 215, 49);
 		btnRemover.addActionListener(new ActionListener() {
@@ -88,9 +94,11 @@ public class TelaAnimador extends JFrame {
 		getContentPane().add(btnRemover);
 	}
 	
+	/* Metodo que cria a tela de insercao de animador */
 	public void inserir() {
 		getContentPane().setLayout(null);
 		
+		/* Criacao dos objetos swing para a interface da tela */
 		lblTitulo = new JLabel("<html><body><center><h1>Tela de Insercao de Animador</h1></center></body></html>");
 		lblTitulo.setVerticalAlignment(SwingConstants.TOP);
 		lblTitulo.setBounds(201, 12, 363, 71);
@@ -132,6 +140,7 @@ public class TelaAnimador extends JFrame {
 		lblTipoAnim.setBounds(119, 294, 147, 15);
 		getContentPane().add(lblTipoAnim);
 		
+		/* Configuracao do botao de voltar para o menu inicial */
 		btnVoltar = new JButton("<");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -146,14 +155,16 @@ public class TelaAnimador extends JFrame {
 		
 		btnCadastrar = new JButton("CADASTRAR");
 		btnCadastrar.addActionListener(new ActionListener() {
+			
+			/* Callback para o o botao de cadastrar banda. Realiza a insercao no banco */
 			public void actionPerformed(ActionEvent e) {
 				
-				String nome = textFieldNome.getText();
-				String telefone = textFieldTelefone.getText();
-				String email = textFieldEmail.getText();			
-				String tipoAnimacao = (String) dropTipoAnim.getSelectedItem();
+				String nome = textFieldNome.getText(); // pega o nome do animador
+				String telefone = textFieldTelefone.getText(); // pega o telefone do animador
+				String email = textFieldEmail.getText(); // pega o email do animador
+				String tipoAnimacao = (String) dropTipoAnim.getSelectedItem(); // pega o tipo de animacao que o animador faz
 				
-				Animador.insert(nome, telefone, email, tipoAnimacao);
+				Animador.insert(nome, telefone, email, tipoAnimacao); // insere no banco
 			}
 		});
 		btnCadastrar.setBounds(526, 313, 117, 25);
@@ -161,9 +172,11 @@ public class TelaAnimador extends JFrame {
 		
 	}
 	
+	/* Metodo que cria a tela de alteracao de animador */
 	public void alterar() {
 		getContentPane().setLayout(null);
 		
+		/* Criacao dos objetos swing para a interface da tela */
 		lblTitulo = new JLabel("<html><body><center><h1>Tela de Alteracao de Animador</h1></center></body></html>");
 		lblTitulo.setVerticalAlignment(SwingConstants.TOP);
 		lblTitulo.setBounds(201, 12, 363, 71);
@@ -204,6 +217,7 @@ public class TelaAnimador extends JFrame {
 		lblTipoAnim.setBounds(119, 294, 147, 15);
 		getContentPane().add(lblTipoAnim);
 		
+		/* Configuracao do botao de voltar para o menu inicial */
 		btnVoltar = new JButton("<");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -218,32 +232,37 @@ public class TelaAnimador extends JFrame {
 		
 		btnCadastrar = new JButton("ALTERAR");
 		btnCadastrar.addActionListener(new ActionListener() {
+			
+			/* Callback para o o botao de alterar animador. Realiza o update no banco */
 			public void actionPerformed(ActionEvent e) {
 				
-				String nome = textFieldNome.getText();
-				String telefone = textFieldTelefone.getText();
-				String email = textFieldEmail.getText();			
-				String tipoAnimacao = (String) dropTipoAnim.getSelectedItem();
+				String nome = textFieldNome.getText(); // pega o nome do animador
+				String telefone = textFieldTelefone.getText(); // pega o telefone do animador
+				String email = textFieldEmail.getText();// pega o email do animador
+				String tipoAnimacao = (String) dropTipoAnim.getSelectedItem(); // pega o tipo de animacao
 				
-				Animador.update(nome, telefone, email, tipoAnimacao);
+				Animador.update(nome, telefone, email, tipoAnimacao); // atualiza o animador no banco
 			}
 		});
 		btnCadastrar.setBounds(524, 312, 117, 25);
 		getContentPane().add(btnCadastrar);
 		
+		/* Configuracao do botao de pesquisar animador */
 		btnPesquisarAnimador = new JButton("Pesquisar Animador");
 		btnPesquisarAnimador.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String nome = textFieldNome.getText();
 				List<String> rs = Animador.select(nome);
 				if (rs != null && rs.size() > 0) {
-					textFieldTelefone.setText(rs.get(0));
-					textFieldEmail.setText(rs.get(1));
-					dropTipoAnim.setSelectedIndex(rs.get(2).equals(options[0])? 0 : 1);
+					textFieldTelefone.setText(rs.get(0)); // seta o campo de telefone
+					textFieldEmail.setText(rs.get(1)); // seta o campo de email
+					dropTipoAnim.setSelectedIndex(rs.get(2).equals(options[0])? 0 : 1); // seta o tipo de animacao escolhido
 					
 					changeVisibility(true);
 					
 				} else {
+					
+					/* Mensagem de erro de animador nao encontrado */
 					textFieldTelefone.setText("");
 					textFieldEmail.setText("");
 					dropTipoAnim.setSelectedIndex(0);
@@ -271,9 +290,11 @@ public class TelaAnimador extends JFrame {
 		
 	}
 	
+	/* Metodo que cria a tela de remocao de animador */
 	public void remover() {
 		getContentPane().setLayout(null);
-		
+
+		/* Criacao dos objetos swing para a interface da tela */
 		lblTitulo = new JLabel("<html><body><center><h1>Tela de Remocao de Animador</h1></center></body></html>");
 		lblTitulo.setVerticalAlignment(SwingConstants.TOP);
 		lblTitulo.setBounds(201, 12, 363, 71);
@@ -317,6 +338,7 @@ public class TelaAnimador extends JFrame {
 		lblTipoAnim.setBounds(119, 294, 147, 15);
 		getContentPane().add(lblTipoAnim);
 		
+		/* Configura botao de voltar para o menu inicial */
 		btnVoltar = new JButton("<");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -329,30 +351,33 @@ public class TelaAnimador extends JFrame {
 		btnVoltar.setBounds(12, 12, 44, 25);
 		getContentPane().add(btnVoltar);
 		
+		/* Configura o botao de remocao de animador */
 		btnCadastrar = new JButton("REMOVER");
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String nome = textFieldNome.getText();
-				Animador.remove(nome);
+				String nome = textFieldNome.getText(); // pega o nome do animador
+				Animador.remove(nome); // remove do banco
 				
 			}
 		});
 		btnCadastrar.setBounds(524, 312, 117, 25);
 		getContentPane().add(btnCadastrar);
 		
+		/* Configuracao do botao de pesquisar animador */
 		btnPesquisarAnimador = new JButton("Pesquisar Animador");
 		btnPesquisarAnimador.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String nome = textFieldNome.getText();
 				List<String> rs = Animador.select(nome);
 				if (rs != null && rs.size() > 0) {
-					textFieldTelefone.setText(rs.get(0));
-					textFieldEmail.setText(rs.get(1));
-					dropTipoAnim.setSelectedIndex(rs.get(2).equals(options[0])? 0 : 1);
+					textFieldTelefone.setText(rs.get(0)); // seta o campo de telefone do animador
+					textFieldEmail.setText(rs.get(1)); // seta o campo de email do animador
+					dropTipoAnim.setSelectedIndex(rs.get(2).equals(options[0])? 0 : 1); // seta o tipo de animacao escolhido
 					
 					changeVisibility(true);
 					
 				} else {
+					/* Mensagem de erro de animador nao encontrado */
 					textFieldTelefone.setText("");
 					textFieldEmail.setText("");
 					dropTipoAnim.setSelectedIndex(0);
