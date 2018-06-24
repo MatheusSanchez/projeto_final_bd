1e67bbc5d2ad5a7f4c7765cfa90376fe788f1bb2
barbaracortes <bcs1698@gmail.com>
2018-06-24 18:25:07 -0300

    sei la mano

--- ProjetoAssessoria/src/main/TelaInicio.java
+++ ProjetoAssessoria/src/main/TelaInicio.java
@@ -1,25 +1,33 @@
 package main;
 
 import java.awt.Container;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import atracao.TelaAnimador;
 import atracao.TelaBanda;
 import buffet.TelaBuffetCasamento;
 import buffet.TelaBuffetInfantil;
+import conexao.Conexao;
 import contratante.TelaContratante;
 import decoracao.TelaDecoracaoCasamento;
 import decoracao.TelaDecoracaoInfantil;
 import festa.TelaFesta;
-import javax.swing.JLabel;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.Point;
 
 @SuppressWarnings("serial")
 public class TelaInicio extends JFrame {
@@ -34,10 +42,6 @@ public class TelaInicio extends JFrame {
 		this.container = c;
 	}
 
-	public void display() {
-		
-	}
-	
 	public void telaInicial() {
 		
 		JButton btnAtualizar = new JButton("Atualizar Banco");
@@ -49,6 +53,12 @@ public class TelaInicio extends JFrame {
 		});
 		
 		JButton btnConsultar = new JButton("Consultar Banco");
+		btnConsultar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Main.novaTela(container);
+				consultaBanco();
+			}
+		});
 		
 		JButton btnGerar = new JButton("Gerar Relatorios");
 		
@@ -182,6 +192,191 @@ public class TelaInicio extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				Main.novaTela(container);
 				telaInicial();
+			}
+		});
+		button.setBounds(12, 12, 44, 25);
+		container.add(button);
+	}
+
+	public void consultaBanco() {
+		container.setLayout(null);
+		
+		JButton btnDecoracaoCasamento = new JButton("Decoracao Casamento");
+		btnDecoracaoCasamento.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaConsulta("Decoracao Casamento");
+			}
+		});
+		btnDecoracaoCasamento.setBounds(161, 176, 198, 25);
+		container.add(btnDecoracaoCasamento);
+		
+		JButton btnDecoracaoInfantil = new JButton("Decoracao Infantil");
+		btnDecoracaoInfantil.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaConsulta("Decoracao Infantil");
+			}
+		});
+		btnDecoracaoInfantil.setBounds(389, 176, 198, 25);
+		container.add(btnDecoracaoInfantil);
+		
+		JButton btnFesta = new JButton("Festa");
+		btnFesta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaConsulta("Festa");
+			}
+		});
+		btnFesta.setBounds(161, 139, 198, 25);
+		container.add(btnFesta);
+		
+		JButton btnContratante = new JButton("Contratante");
+		btnContratante.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaConsulta("Contratante");
+			}
+		});
+		btnContratante.setBounds(389, 139, 198, 25);
+		container.add(btnContratante);
+		
+		JButton btnBanda = new JButton("Banda");
+		btnBanda.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaConsulta("Banda");
+			}
+		});
+		btnBanda.setBounds(161, 213, 198, 25);
+		container.add(btnBanda);
+		
+		JButton btnAnimador = new JButton("Animador");
+		btnAnimador.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaConsulta("Animador");
+			}
+		});
+		btnAnimador.setBounds(389, 213, 198, 25);
+		container.add(btnAnimador);
+		
+		JButton btnBuffetCasamento = new JButton("Buffet Casamento");
+		btnBuffetCasamento.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaConsulta("Buffet Casamento");
+			}
+		});
+		btnBuffetCasamento.setBounds(161, 250, 198, 25);
+		container.add(btnBuffetCasamento);
+		
+		JButton btnBuffetInfantil = new JButton("Buffet Infantil");
+		btnBuffetInfantil.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaConsulta("Buffet Infantil");
+			}
+		});
+		btnBuffetInfantil.setBounds(389, 250, 198, 25);
+		container.add(btnBuffetInfantil);
+		
+		JButton button = new JButton("<");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				telaInicial();
+			}
+		});
+		button.setBounds(12, 12, 44, 25);
+		container.add(button);
+	}
+	
+	public void telaConsulta(String tabela) {
+		getContentPane().setLayout(null);
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setBounds(10, 114, 732, 316);
+		getContentPane().add(editorPane);
+		
+		JLabel lblTitle_1 = new JLabel("<html><h1>Cadastros de " + tabela + "</h1></html>");
+		lblTitle_1.setBounds(10, 25, 732, 42);
+		getContentPane().add(lblTitle_1);
+		
+		JTextField lblColunas = new JTextField("");
+		lblColunas.setBounds(10, 78, 732, 25);
+		getContentPane().add(lblColunas);
+		
+		String s = "";
+		
+		Connection c = Conexao.getInstance();
+		
+		try {
+			switch (tabela) {
+				case "Decoracao Casamento":
+					tabela = "DECORACAOCASAMENTO";
+					break;
+				case "Decoracao Infantil":
+					tabela = "DECORACAOINFANTIL";
+					break;
+				case "Buffet Casamento":
+					tabela = "BUFFETCASAMENTO";
+					break;
+				case "Buffet Infantil":
+					tabela = "BUFFETINFANTIL";
+					break;
+				case "Contratante":
+					tabela = "CONTRATANTE";
+					break;
+				case "Banda":
+					tabela = "BANDA";
+					break;
+				case "Animador":
+					tabela = "ANIMADOR";
+					break;
+				default:
+					break;
+			}
+			
+			String sql = "select column_name from user_tab_columns where table_name = ?";
+			PreparedStatement pstm = c.prepareStatement(sql);
+			pstm.setString(1, tabela);
+			ResultSet rs = pstm.executeQuery();
+			
+			List<String> colunas = new ArrayList<String>();
+			while (rs.next()) {
+				colunas.add(rs.getString(1));
+			}
+			
+			String aux = "";
+			for (String string : colunas) {
+				aux += string + "\t";
+			}
+			
+			lblColunas.setText(aux);
+			
+			sql = "select * from " + tabela;
+			pstm = c.prepareStatement(sql);
+			rs = pstm.executeQuery();
+			
+			while (rs.next()) {
+				for (String string : colunas) {
+					s += rs.getString(string) + "\t";
+				}
+				s += "\n";
+			}
+			
+			editorPane.setText(s);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		JButton button = new JButton("<");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.novaTela(container);
+				consultaBanco();
 			}
 		});
 		button.setBounds(12, 12, 44, 25);
